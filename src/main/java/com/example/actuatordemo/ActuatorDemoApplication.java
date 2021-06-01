3b4df782fcc314e10acda375b7628228ecaeef18
ibmpariwesh <80327691+ibmpariwesh@users.noreply.github.com>
2021-06-01 21:06:46 +0530

    Update ActuatorDemoApplication.java

--- src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
+++ src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActuatorDemoApplication {
+public class ActuatorDemoApplication {hehe :)
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
